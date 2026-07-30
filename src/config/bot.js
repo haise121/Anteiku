bea9980c807bc205bd034ec026f5775ddbd41254
haise121 <nxlan.rl@gmail.com>
2026-07-31 01:45:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "jte stalk",     // this is what people actually see
+        state: "stalking",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
