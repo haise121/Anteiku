6c68eef267b9890c335127a33f08f58a7115ac0f
haise121 <nxlan.rl@gmail.com>
2026-07-31 12:57:17 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Watching", // required by Discord API, not shown in the client
-        state: "Tokyo Ghoul",     // this is what people actually see
+        name: "Tokyo Ghoul", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
         type: 1,               // Custom
       },
     ],
